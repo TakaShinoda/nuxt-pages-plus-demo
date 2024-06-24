10d1048f210fad2e00188f98116600ce82869954
Takahiro <tkhr_621@docomo.ne.jp>
2024-06-25 00:46:58 +0900

    デモ用のUIを実装

--- app.vue
+++ app.vue
@@ -1,6 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+  <AppHeader />
+  <div class="mx-auto max-w-7xl pt-16 lg:flex lg:gap-x-16 lg:px-8">
+    <AppNav />
+    <main class="px-4 py-16 sm:px-6 lg:flex-auto lg:px-0 lg:py-20">
+      <TeamMembers />
+    </main>
   </div>
 </template>
---
+++ components/AppNav.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+import {
+  BellIcon,
+  FingerPrintIcon,
+  UserCircleIcon,
+  UsersIcon,
+} from '@heroicons/vue/24/outline'
+
+const secondaryNavigation = [
+  { name: 'General', href: '#', icon: UserCircleIcon, current: false },
+  { name: 'Security', href: '#', icon: FingerPrintIcon, current: false },
+  { name: 'Notifications', href: '#', icon: BellIcon, current: false },
+  { name: 'Team', href: '#', icon: UsersIcon, current: true },
+] as const
+</script>
+
+<template>
+  <aside class="flex overflow-x-auto border-b border-gray-900/5 py-4 lg:block lg:w-64 lg:flex-none lg:border-0 lg:py-20">
+    <nav class="flex-none px-4 sm:px-6 lg:px-0">
+      <ul role="list" class="flex gap-x-3 gap-y-1 whitespace-nowrap lg:flex-col">
+        <li v-for="item in secondaryNavigation" :key="item.name">
+          <a :href="item.href" :class="[item.current ? 'bg-gray-50 text-sky-600' : 'text-gray-700 hover:bg-gray-50 hover:text-sky-600', 'group flex gap-x-3 rounded-md py-2 pl-2 pr-3 text-sm font-semibold leading-6']">
+            <component :is="item.icon" :class="[item.current ? 'text-sky-600' : 'text-gray-400 group-hover:text-sky-600', 'h-6 w-6 shrink-0']" aria-hidden="true" />
+            {{ item.name }}
+          </a>
+        </li>
+      </ul>
+    </nav>
+  </aside>
+</template>
