9c33399c871b59c524b5df8248b050c8de8c0971
Takahiro <tkhr_621@docomo.ne.jp>
2024-06-25 02:49:44 +0900

    ページを作成

---
+++ pages/notifications/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <h1 class="sr-only">Notifications Settings</h1>
+  <div class="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
+    <div>
+      <h2 class="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
+      </div>
+  </div>
+</template>
