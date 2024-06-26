080d47e7119fec331ff2ad619e50dcb777e1c782
Takahiro <tkhr_621@docomo.ne.jp>
2024-06-27 01:46:19 +0900

    create page

--- pages/team/index.vue
+++ pages/team/index.vue
@@ -2,26 +2,21 @@
 import { ref } from 'vue'
 
 const open = ref(false)
-const handleClick = () => {
-  console.log('handleClick')
-  open.value = true
-}
 </script>
 
 <template>
-  <!-- <h1 class="sr-only">Team Settings</h1> -->
-  <h1 class="sr-only">Notifications Settings (legacy)</h1>
-  <LegacyDialog v-model="open" />
+  <h1 class="sr-only">Team Settings</h1>
+  <MemberAddDialog v-model="open" />
   <div class="mx-auto max-w-2xl space-y-16 sm:space-y-20 lg:mx-0 lg:max-w-none">
     <div>
-      <!-- <h2 class="text-base font-semibold leading-7 text-gray-900">Team Members</h2> -->
-      <h2 class="text-base font-semibold leading-7 text-gray-900">Notifications</h2>
+      <h2 class="text-base font-semibold leading-7 text-gray-900">Team Members</h2>
       <dl class="mt-6 space-y-6 divide-y divide-gray-100 border-t border-gray-200 text-sm leading-6">
         <div class="pt-6 sm:flex">
           <dt class="font-medium text-gray-900 sm:w-64 sm:flex-none sm:pr-6">メンバー追加</dt>
           <dd class="mt-1 flex justify-between gap-x-6 sm:mt-0 sm:flex-auto">
             <div class="text-gray-900">チームメンバーを追加します</div>
-            <button @click="handleClick" type="button" class="font-semibold text-sky-600 hover:text-sky-500">Add</button>
+            <PlusModalLink to="/team/add" class="font-semibold text-sky-600 hover:text-sky-500">Add</PlusModalLink>
+            <PlusModalPage name="dialog" />
           </dd>
         </div>
         <div class="pt-6 sm:flex">
